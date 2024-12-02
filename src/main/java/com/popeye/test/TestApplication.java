bc79d34920d27d46515f002b187330ce1c943458
dongseong.cho <popeye7912@gmail.com>
2024-12-02 11:22:10 +0900

    test

---
+++ src/main/java/com/popeye/test/TestApplication.java
@@ -0,0 +1,16 @@
+package com.popeye.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		log.info("### Popeye Application Start args : {} ###", args);
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
