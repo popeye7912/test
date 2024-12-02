22de0ec7a76104ff71a4c678af14927711b58598
dongseong.cho <popeye7912@gmail.com>
2024-12-02 15:49:59 +0900

    test log

---
+++ src/main/java/com/popeye/test/main/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.popeye.test.main.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/")
+    public void test() throws Exception {
+
+        logger.trace("TRACE Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("WARN Level 테스트");
+        logger.error("ERROR Level 테스트");
+
+    }
+}
